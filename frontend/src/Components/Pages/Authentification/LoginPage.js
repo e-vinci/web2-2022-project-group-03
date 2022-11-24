56927ed8440ad945f78afe89752b57a06094fc9f
cdhaeyere <corentin.dhaeyere@student.vinci.be>
2022-11-24 11:59:35 +0100

    feat : login and register page gen

---
+++ frontend/src/Components/Pages/Authentification/LoginPage.js
@@ -0,0 +1,37 @@
+const LoginPage = () => {
+
+    const main = document.querySelector('main');
+
+    const section = document.createElement('section');
+    main.appendChild(section);
+
+    const logo = document.createElement('img');
+    logo.src = ''; // TODO : Add logo
+
+    section.appendChild(logo);
+
+    const form = document.createElement('form');
+    form.setAttribute('method', 'post');
+
+    const usernameInput = document.createElement('input');
+    usernameInput.setAttribute('type', 'text');
+    usernameInput.setAttribute('name', 'username');
+    usernameInput.setAttribute('placeholder', 'Username');
+
+    const passwordInput = document.createElement('input');
+    passwordInput.setAttribute('type', 'password');
+    passwordInput.setAttribute('name', 'password');
+    passwordInput.setAttribute('placeholder', 'Password');
+
+    const submitButton = document.createElement('button');
+    submitButton.setAttribute('type', 'submit');
+    submitButton.innerText = 'Login';
+
+    form.appendChild(usernameInput);
+    form.appendChild(passwordInput);
+    form.appendChild(submitButton);
+
+    section.appendChild(form);
+}
+
+export default LoginPage;
--- frontend/src/Components/Pages/Authentification/RegisterPage.js
+++ frontend/src/Components/Pages/Authentification/RegisterPage.js
@@ -1,5 +1,58 @@
 const RegisterPage = () => {
+  const main = document.querySelector("main");
 
+  const section = document.createElement("section");
+  main.appendChild(section);
+
+  const logo = document.createElement("img");
+  logo.src = ""; // TODO : Add logo
+
+  section.appendChild(logo);
+
+  const form = document.createElement("form");
+  form.setAttribute("method", "post");
+
+  const usernameInput = document.createElement("input");
+  usernameInput.setAttribute("type", "text");
+  usernameInput.setAttribute("name", "username");
+  usernameInput.setAttribute("placeholder", "Username");
+
+  const emailInput = document.createElement("input");
+  emailInput.setAttribute("type", "email");
+  emailInput.setAttribute("name", "email");
+  emailInput.setAttribute("placeholder", "Email");
+
+  const passwordInput = document.createElement("input");
+  passwordInput.setAttribute("type", "password");
+  passwordInput.setAttribute("name", "password");
+  passwordInput.setAttribute("placeholder", "Password");
+
+  const passwordConfirmationInput = document.createElement("input");
+  passwordConfirmationInput.setAttribute("type", "password");
+  passwordConfirmationInput.setAttribute("name", "passwordConfirmation");
+  passwordConfirmationInput.setAttribute("placeholder", "Password Confirmation");
+
+  const termsOfUseCheckbox = document.createElement("input");
+  termsOfUseCheckbox.setAttribute("type", "checkbox");
+  termsOfUseCheckbox.setAttribute("name", "termsOfUse");
+  termsOfUseCheckbox.setAttribute("value", "termsOfUse");
+
+  const termsOfUseLabel = document.createElement("label");
+  termsOfUseLabel.innerText = "I agree to the Terms of Use";
+
+  const submitButton = document.createElement("button");
+  submitButton.setAttribute("type", "submit");
+  submitButton.innerText = "Register";
+
+  form.appendChild(usernameInput);
+  form.appendChild(emailInput);
+  form.appendChild(passwordInput);
+  form.appendChild(passwordConfirmationInput);
+  form.appendChild(termsOfUseCheckbox);
+  form.appendChild(termsOfUseLabel);
+  form.appendChild(submitButton);
+
+  section.appendChild(form);
 }
 
 export default RegisterPage;
