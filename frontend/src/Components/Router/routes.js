2a72eea27dd0c479787798a8234811e2e835d58f
Nitroc1701 <nitrocdev@gmail.com>
2022-11-26 14:29:07 +0100

    feat: add /leaderboard route

--- frontend/src/Components/Router/routes.js
+++ frontend/src/Components/Router/routes.js
@@ -2,6 +2,7 @@ import HomePage from "../Pages/HomePage/HomePage";
 import LoginPage from "../Pages/Authentification/LoginPage";
 import RegisterPage from "../Pages/Authentification/RegisterPage";
 import GamePage from "../Pages/Game/GamePage";
+import LeaderboardPage from "../Pages/Leaderboard/LeaderboardPage";
 
 
 const routes = {
