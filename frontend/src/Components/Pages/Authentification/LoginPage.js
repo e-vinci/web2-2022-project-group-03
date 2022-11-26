2a32739898797cf8efb3659dc96e9a2c359cd441
Nitroc1701 <nitrocdev@gmail.com>
2022-11-26 14:51:14 +0100

    feat: handle login form

--- frontend/src/Components/Pages/Authentification/LoginPage.js
+++ frontend/src/Components/Pages/Authentification/LoginPage.js
@@ -1,4 +1,6 @@
 import {clearPage} from "../../../utils/render";
+import Navigate from "../../Router/Navigate";
+import { setAuthenticatedUser } from "../../../utils/auths";
 
 const LoginPage = () => {
     clearPage();
@@ -20,11 +22,20 @@ const LoginPage = () => {
     usernameInput.setAttribute('type', 'text');
     usernameInput.setAttribute('name', 'username');
     usernameInput.setAttribute('placeholder', 'Username');
+    usernameInput.setAttribute('required', 'required');
 
     const passwordInput = document.createElement('input');
     passwordInput.setAttribute('type', 'password');
     passwordInput.setAttribute('name', 'password');
     passwordInput.setAttribute('placeholder', 'Password');
+    passwordInput.setAttribute('required', 'required');
+
+    const rememberMeCheckbox = document.createElement('input');
+    rememberMeCheckbox.setAttribute('type', 'checkbox');
+    rememberMeCheckbox.setAttribute('name', 'rememberMe');
+
+    const rememberMeLabel = document.createElement('label');
+    rememberMeLabel.innerText = 'Remember me';
 
     const submitButton = document.createElement('button');
     submitButton.setAttribute('type', 'submit');
@@ -32,9 +43,41 @@ const LoginPage = () => {
 
     form.appendChild(usernameInput);
     form.appendChild(passwordInput);
+    form.appendChild(rememberMeCheckbox);
+    form.appendChild(rememberMeLabel);
     form.appendChild(submitButton);
 
     section.appendChild(form);
+
+    form.addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(form);
+        const data = Object.fromEntries(formData);
+
+        const {username, password, rememberMe} = data;
+
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username,
+                password,
+            })
+        }
+
+        const response = await fetch('http://localhost:3000/api/auth/login', options);
+
+        if (!response.ok) throw new Error(`fetch error: ${response.status}`);
+
+        const authenticatedUser = await response.json();
+
+        setAuthenticatedUser(authenticatedUser, rememberMe);
+
+        Navigate('/');
+    });
 }
 
 export default LoginPage;
