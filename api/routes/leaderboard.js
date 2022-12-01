491f54c29eaf9dd019c99e04bd70248fbc7c4277
Nitroc1701 <nitrocdev@gmail.com>
2022-12-01 16:44:23 +0100

    feat: leaderboard API

--- api/routes/leaderboard.js
+++ api/routes/leaderboard.js
@@ -5,22 +5,28 @@ const router = express.Router();
 
 const { parse, serialize } = require("../utils/json");
 
-const jsonDbPath = path.join('./../data/leaderboard.json');
+const jsonDbPath = path.join(__dirname, '/../data/leaderboard.json');
 
-router.post('/{level}', (req, res) => {
+router.post('/', (req, res) => {
   const leaderboard = parse(jsonDbPath);
 
-  const { level } = req.query.params;
-
-  console.log(level);
+  return res.json(leaderboard.sort((a, b) => a.time - b.time));
 });
 
-router.post('/{user}', (req, res) => {
+router.post('/levels/:levelId', (req, res) => {
   const leaderboard = parse(jsonDbPath);
 
-  const { user } = req.query.params;
+  const { levelId } = req.params;
 
-  console.log(user);
+  return res.json(leaderboard.filter((element) => element.level === levelId).sort((a, b) => a.time - b.time));
+});
+
+router.post('/users/:user_uuid', (req, res) => {
+  const leaderboard = parse(jsonDbPath);
+
+  const { user_uuid } = req.params;
+
+  return res.json(leaderboard.filter((element) => element.user_uuid === user_uuid).sort((a, b) => a.time - b.time));
 });
 
 module.exports = router;
