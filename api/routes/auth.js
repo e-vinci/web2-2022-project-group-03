4ebd73db2e0868915fbd89594b1a250add9f1d37
Nitroc1701 <nitrocdev@gmail.com>
2022-12-01 16:44:08 +0100

    feat: same username case

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,13 +2,25 @@ const express = require('express');
 
 const router = express.Router();
 
+const path = require("node:path");
+
 const { register, login } = require('../models/auth');
+const { parse } = require("../utils/json");
+
+const jsonDbPath = path.join(__dirname, '/../data/users.json');
 
 router.post('/register', async (req, res) => {
   const username = req?.body?.username?.length !== 0 ? req.body.username : undefined;
   const password = req?.body?.password?.length !== 0 ? req.body.password : undefined;
 
   if (!username || !password) return res.sendStatus(400);
+
+  const users = parse(jsonDbPath);
+
+  // eslint-disable-next-line consistent-return
+  users.forEach((user) => {
+    if (user.username === username) return res.sendStatus(409);
+  });
 
   const authenticatedUser = await register(username, password);
 
