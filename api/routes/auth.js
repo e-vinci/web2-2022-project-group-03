315c5f4ecd0768e587e0fd34d8777b1e5b918e2a
miguelDeSaAdegas <miguel.desaadegas@student.vinci.be>
2022-12-01 12:46:10 +0100

    test login et register

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const router = express.Router();
 
 const { register, login } = require('../models/auth');
@@ -7,11 +8,11 @@ router.post('/register', async (req, res) => {
   const username = req?.body?.username?.length !== 0 ? req.body.username : undefined;
   const password = req?.body?.password?.length !== 0 ? req.body.password : undefined;
 
-  if (!username || !password) return res.status(400);
+  if (!username || !password) return res.sendStatus(400);
 
   const authenticatedUser = await register(username, password);
 
-  if (!authenticatedUser) return res.status(409);
+  if (!authenticatedUser) return res.sendStatus(401);
 
   return res.json(authenticatedUser);
 });
@@ -24,7 +25,7 @@ router.post('/login', async (req, res) => {
 
   const authenticatedUser = await login(username, password);
 
-  if (!authenticatedUser) return res.sendStatus(409);
+  if (!authenticatedUser) return res.sendStatus(401);
 
   return res.json(authenticatedUser);
 });
