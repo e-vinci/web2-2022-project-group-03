9375d7e3ebe3e987443f6a516e84fd89a81f895f
cdhaeyere <corentin.dhaeyere@student.vinci.be>
2022-12-02 11:21:18 +0100

    fix: errors in english

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -15,25 +15,30 @@ router.post('/register', async (req, res) => {
   const username = req?.body?.username?.length !== 0 ? req.body.username : undefined;
   const password = req?.body?.password?.length !== 0 ? req.body.password : undefined;
 
-  if (!username || !password) return res.status(400).json({ message: 'Nom d\'utilisateur ou mot de passe manquant !' });
+  if (!username || !password) return res.status(400).json({ message: 'Username or password missing !' });
 
   const checker = new Checker();
   checker.min_length = 8;
   checker.disallowPasswords(true, true, 3);
+  checker.disallowNames(true, true, 3);
+  checker.disallowWords(true, true, 3);
+  checker.allowed_symbols = '!@#$%^&*()_+{}|:"<>?[];\',./`~';
+  checker.allowed_numbers = '1234567890';
+  checker.allowed_letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
   if (!checker.check(password)) {
     const errors = [];
     checker.errors.forEach((error) => {
       errors.push({ error: error.message });
     });
-    return res.status(400).json({ error: 'Votre mot de passe doit contenir au moins 8 caractères et pas être obvious' });
+    return res.status(400).json({ error: 'Your password must be 8 characters and not obvious !' });
   }
 
   const users = parse(jsonDbPath);
 
   // eslint-disable-next-line consistent-return
   users.forEach((user) => {
-    if (user.username === username) return res.status(409).json({ error: 'Ce nom d\'utilisateur est déjà utilisé' });
+    if (user.username === username) return res.status(409).json({ error: 'Username already exists !' });
   });
 
   const authenticatedUser = await register(username, password);
@@ -47,11 +52,11 @@ router.post('/login', async (req, res) => {
   const username = req?.body?.username?.length !== 0 ? req.body.username : undefined;
   const password = req?.body?.password?.length !== 0 ? req.body.password : undefined;
 
-  if (!username || !password) return res.status(400).json({ error: 'Nom d\'utilisateur ou mot de passe manquant !' });
+  if (!username || !password) return res.status(400).json({ error: 'Username or password missing !' });
 
   const authenticatedUser = await login(username, password);
 
-  if (!authenticatedUser) return res.status(401).json({ error: 'Mauvais identifiant ou mot de passe !' });
+  if (!authenticatedUser) return res.status(401).json({ error: 'Wrong username or password !' });
 
   return res.json(authenticatedUser);
 });
