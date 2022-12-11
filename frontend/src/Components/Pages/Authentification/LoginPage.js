c5bbd68458b0bd9cad071898715b80c260339ea6
Nitroc1701 <nitrocdev@gmail.com>
2022-12-11 14:21:43 +0100

    refactor: error.js utils

--- frontend/src/Components/Pages/Authentification/LoginPage.js
+++ frontend/src/Components/Pages/Authentification/LoginPage.js
@@ -1,6 +1,7 @@
 import {clearPage} from "../../../utils/render";
 import Navigate from "../../Router/Navigate";
 import { setAuthenticatedUser } from "../../../utils/auths";
+import showErrorMessage from "../../../utils/error";
 
 const LoginPage = () => {
     clearPage();
@@ -106,25 +107,6 @@ const LoginPage = () => {
 
         Navigate('/');
     });
-}
-
-function showErrorMessage(message, element) {
-    const div = document.createElement('div');
-    div.classList.add('alert');
-    element.appendChild(div);
-
-    const span = document.createElement('span');
-    span.textContent = message;
-    span.classList.add('msg');
-    div.appendChild(span);
-
-    div.classList.add('show');
-    div.classList.remove('hide');
-    div.classList.add('showAlert');
-    setTimeout(() => {
-        div.classList.remove('show');
-        div.classList.add('hide');
-    }, 5000);
 }
 
 export default LoginPage;
--- frontend/src/Components/Pages/Authentification/RegisterPage.js
+++ frontend/src/Components/Pages/Authentification/RegisterPage.js
@@ -1,6 +1,7 @@
 import {clearPage} from "../../../utils/render";
 import {setAuthenticatedUser} from "../../../utils/auths";
 import Navigate from "../../Router/Navigate";
+import showErrorMessage from "../../../utils/error";
 
 const RegisterPage = async () => {
     clearPage();
@@ -134,25 +135,6 @@ const RegisterPage = async () => {
 
         Navigate("/");
     });
-}
-
-function showErrorMessage(message, element) {
-    const div = document.createElement('div');
-    div.classList.add('alert');
-    element.appendChild(div);
-
-    const span = document.createElement('span');
-    span.textContent = message;
-    span.classList.add('msg');
-    div.appendChild(span);
-
-    div.classList.add('show');
-    div.classList.remove('hide');
-    div.classList.add('showAlert');
-    setTimeout(() => {
-        div.classList.remove('show');
-        div.classList.add('hide');
-    }, 5000);
 }
 
 export default RegisterPage;
