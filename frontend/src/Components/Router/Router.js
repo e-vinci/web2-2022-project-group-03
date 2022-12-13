1493b61f8efda8ca29e163c3555e2f6de138628b
francois.vandeputte <francois.vandeputte@student.vinci.be>
2022-12-13 22:46:44 +0100

    new boilerplate

--- frontend/src/Components/Router/Router.js
+++ frontend/src/Components/Router/Router.js
@@ -1,30 +1,14 @@
+import { removePathPrefix } from '../../utils/path-prefix';
 import routes from './routes';
 
 const Router = () => {
     onFrontendLoad();
-    onNavBarClick();
     onHistoryChange();
 };
 
-function onNavBarClick() {
-    const navItems = document.querySelectorAll('.nav-link');
-
-    navItems.forEach((item) => {
-        item.addEventListener('click', (e) => {
-            e.preventDefault();
-            const uri = e.target?.dataset?.uri;
-            const componentToRender = routes[uri];
-            if (!componentToRender) throw Error(`The ${uri} ressource does not exist.`);
-
-            componentToRender();
-            window.history.pushState({}, '', uri);
-        });
-    });
-}
-
 function onHistoryChange() {
     window.addEventListener('popstate', () => {
-        const uri = window.location.pathname;
+        const uri = removePathPrefix(window.location.pathname);
         const componentToRender = routes[uri];
         componentToRender();
     });
@@ -32,9 +16,10 @@ function onHistoryChange() {
 
 function onFrontendLoad() {
     window.addEventListener('load', () => {
-        const uri = window.location.pathname;
+        const uri = removePathPrefix(window.location.pathname);
         const componentToRender = routes[uri];
         if (!componentToRender) throw Error(`The ${uri} ressource does not exist.`);
+
         componentToRender();
     });
 }
