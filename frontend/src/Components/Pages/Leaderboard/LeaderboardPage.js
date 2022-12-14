431b23fe935897bf685f6aaa68d8cae7f8b89e2c
Nitroc1701 <nitrocdev@gmail.com>
2022-12-14 22:01:28 +0100

    feat: get leaderboard from api

--- frontend/src/Components/Pages/Leaderboard/LeaderboardPage.js
+++ frontend/src/Components/Pages/Leaderboard/LeaderboardPage.js
@@ -10,41 +10,8 @@ const LeaderboardPage = async () => {
         },
     });
 
-    const users = await result.json();
-
-    console.log(users);
-
-    const main = document.querySelector('main');
-
-    let table = `
-        <table>
-            <thead>
-                <tr>
-                    <th>Rang</th>
-                    <th>Nom</th>
-                    <th>Temps</th>
-                    <th>Niveau</th>
-                </tr>
-            </thead>
-            <tbody>
-    `;
-
-    for (let i = 0; i < users.length; i + 1) {
-        table += `
-            <tr>
-                <td>${users[i].username}</td>
-                <td>${users[i].time}</td>
-                <td>${users[i].level}</td>
-            </tr>
-        `;
-    }
-
-    table += `
-            </tbody>
-        </table>
-    `;
-
-    main.innerHTML = table;
+    const leaderboard = await result.json();
+    console.log(leaderboard);
 }
 
 export default LeaderboardPage;
