6bd0f5fa17722f585b20a3de3fd02d804b747dbe
ractouf <francois.vandeputte@student.vinci.be>
2022-12-15 12:41:00 +0100

    Track a suivre pour a la maison

--- frontend/src/Components/Babylon/Game.js
+++ frontend/src/Components/Babylon/Game.js
@@ -337,5 +337,37 @@ export default class Game {
         const result = await SceneLoader.ImportMeshAsync(null, mcqueen);
         const car = result.meshes[0];
         car.position = new Vector3(22.5, 3, -30);
+
+        /*
+        const slide = function (turn, dist) { //after covering dist apply turn
+            this.turn = turn;
+            this.dist = dist;
+        }
+
+        const track = [];
+        track.push(new slide(Math.PI / 2, 4));
+        track.push(new slide(3 * Math.PI / 4, 8));
+        track.push(new slide(3 * Math.PI / 4, 8 + 4 * Math.sqrt(2)));
+
+        let distance = 0;
+        let step = 0.05;
+        let p = 0;
+
+        scene.onBeforeRenderObservable.add(() => {
+            sphere.movePOV(0, 0, step);
+            distance += step;
+
+            if (distance > track[p].dist) {
+                sphere.rotate(BABYLON.Axis.Y, track[p].turn, BABYLON.Space.LOCAL);
+                p +=1;
+                p %= track.length;
+                if (p === 0) {
+                    distance = 0;
+                    sphere.position = new BABYLON.Vector3(2, 0, 2); //reset to initial conditions
+                    sphere.rotation = BABYLON.Vector3.Zero();//prevents error accumulation
+                }
+            }
+        });
+        */
     }
 }
